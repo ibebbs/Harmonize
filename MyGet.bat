dcb25033a98082cf11362af6c174b14d5e38aea6
ibebbs <github@bebbs.co.uk>
2014-06-07 20:16:41 +0100

    Ok, should now package SignalR.Client correctly

--- MyGet.bat
+++ MyGet.bat
@@ -4,6 +4,10 @@ if "%config%" == "" (
    set config=Release
 )
 
+if "%PackageVersion%" == "" (
+  set PackageVersion=1.0.0.0
+)
+
 set version=
 if not "%PackageVersion%" == "" (
    set version=-Version %PackageVersion%
@@ -16,4 +20,4 @@ REM Build
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Bebbs.Harmonize.sln /p:RunOctoPack=true /p:OctoPackPublishPackageToFileShare=..\Services /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 REM Package
-.nuget\nuget.exe pack Bebbs.Harmonize.With.Messaging.Via.SignalR.Client\Bebbs.Harmonize.With.Messaging.Via.SignalR.Client.csproj -OutputDirectory .\Services -Version %PackageVersion%
+.nuget\nuget.exe pack Bebbs.Harmonize.With.Messaging.Via.SignalR.Client\Bebbs.Harmonize.With.Messaging.Via.SignalR.Client.csproj -OutputDirectory .\Services -Version %PackageVersion% -IncludeReferencedProjects
