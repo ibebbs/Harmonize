825991249bb94a5581a1bbccd14180b89cc529eb
ibebbs <github@bebbs.co.uk>
2014-06-28 15:08:01 +0100

    Integrated SignalR client debugging

--- Bebbs.Harmonize.With.Messaging.Via.SignalR.Client/Hub/Factory.cs
+++ Bebbs.Harmonize.With.Messaging.Via.SignalR.Client/Hub/Factory.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNet.SignalR.Client;
+using System.IO;
 
 namespace Bebbs.Harmonize.With.Messaging.Via.SignalR.Client.Hub
 {
     public interface IFactory
     {
         IInstance Create();
+        IInstance Create(TextWriter hubDebug);
     }
 
     internal class Factory : IFactory
@@ -16,12 +18,24 @@ namespace Bebbs.Harmonize.With.Messaging.Via.SignalR.Client.Hub
             _settings = settings;
         }
 
-        public IInstance Create()
+        public IInstance Create(TextWriter hubDebug)
         {
             HubConnection hubConnection = new HubConnection(_settings.HarmonizeSignalRUrl);
+
+            if (hubDebug != null)
+            {
+                hubConnection.TraceLevel = TraceLevels.All;
+                hubConnection.TraceWriter = hubDebug;
+            }
+
             IHubProxy hubProxy = hubConnection.CreateHubProxy(_settings.HarmonizeHubName);
 
             return new Instance(hubConnection, hubProxy);
+        }
+
+        public IInstance Create()
+        {
+            return Create(null);
         }
     }
 }
